59dd9a855da117e96cc33d93bf2621301e2c7de8
DogwishX <zefiru@protonmail.com>
2022-07-22 16:14:52 +0100

    Add `resizeCanvas()` `Closes #1`

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+window.addEventListener("resize", resizeCanvas);
+
+// Game settings
+const gameWidth = 400;
+const gameHeight = 380;
 const blockSize = 10;
 
+// Snake
 const snake = {
   head: {
-    x: 50,
-    y: 50,
+    x: canvas.width / 2,
+    y: canvas.height / 2,
     height: blockSize,
     width: blockSize,
   },
 };
 
 drawGame();
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth < gameWidth ? window.innerWidth : gameWidth;
+  centerSnake();
+}
+
+function centerSnake() {
+  snake.head.x = canvas.width / 2;
+  snake.head.y = canvas.height / 2;
+  drawSnake();
+}
 
 function drawGame() {
   window.requestAnimationFrame(drawGame);
